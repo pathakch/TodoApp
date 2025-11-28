87b6b16e7c07b9b124c1a01cfcd1fc0e95ced050
PATHAK <chandan.kumar.pathak@sap.com>
2025-11-28 19:37:05 +0530

    added postgresql db url from render db

--- todo/database.py
+++ todo/database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args= {'check_same_thread':False})
+SQLALCHEMY_DATABASE_URL = 'postgresql://tododb_2f35_user:0nqzMXyHgoBGXKf8eGDLUsW0h8ugKrnc@dpg-d4kqkfs9c44c73f4saa0-a/tododb_2f35'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args= {'check_same_thread':False})
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
